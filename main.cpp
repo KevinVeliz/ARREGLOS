42463f3fb9b0af29aac4ebfe98c13cedd768eece
Kevin Veliz <mario-veliz258934@hotmail.com>
2020-07-23 15:20:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+void imprimir(int a[]);
+
+int main()
+{
+
+    int arreglo[50];
+    int n;
+    imprimir(arreglo);
+
+    return 0;
+}
+
+
+void imprimir(int a[]  )
+{
+    int i;
+    int arreglo[50];
+    int num=0;
+    cout<<" LOS PARES SON "<<endl;
+    for(i=0;i<50;i++)
+    {
+      num=num+1;
+      if (i%2!=0 && arreglo[i]%2==0)
+      {
+        arreglo[i]=num;
+        
+        cout<<" UB "<<" ["<<i<<"]:"<<arreglo[i]<<endl; 
+      }
+       
+    }
+}
